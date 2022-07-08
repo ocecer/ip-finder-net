0de830b8fbda66fe82debc3eb1222d204d3e3eb8
ocecer <54540749+ocecer@users.noreply.github.com>
2022-07-08 08:01:59 +0200

    Update settings.py

--- ipfinder/settings.py
+++ ipfinder/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/images/"
