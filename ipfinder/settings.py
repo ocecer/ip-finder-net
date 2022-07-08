555bffdb717dc10e816447e9d2179a8ba7c53444
ocecer <54540749+ocecer@users.noreply.github.com>
2022-07-08 08:35:49 +0200

    Update settings.py

--- ipfinder/settings.py
+++ ipfinder/settings.py
@@ -139,16 +139,16 @@ MEDIA_URL = "/images/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# heroku changes
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+# # heroku changes
+# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+# # Extra lookup directories for collectstatic to find static files
+# STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
 
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# #  Add configuration for static files storage using whitenoise
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
